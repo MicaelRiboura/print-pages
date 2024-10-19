985a82dd2f06809a14a27daf26e51743924cc539
MicaelRiboura <micaelribouradossantos@gmail.com>
2024-10-18 23:13:03 -0300

    feat: create version 2 with generated pages for all months

--- scripts.js
+++ scripts.js
@@ -1,24 +1,80 @@
+const dictMonth = {
+    0: 'Janeiro',
+    1: 'Fevereiro',
+    2: 'Março',
+    3: 'Abril',
+    4: 'Maio',
+    5: 'Junho',
+    6: 'Julho',
+    7: 'Agosto',
+    8: 'Setembro',
+    9: 'Outubro',
+    10: 'Novembro',
+    11: 'Dezembro',
+};
+
 document.getElementById('createForm').addEventListener('submit', (event) => {
     event.preventDefault();
-    const start = document.getElementById('start').value;
-    const end = document.getElementById('end').value;
-    const paresInput = document.getElementById('pares');
-    const imparesInput = document.getElementById('impares');
 
-    const pages = [];
-    for (let i = parseInt(start); i <= parseInt(end); i++) {
-        pages.push(i);
+    const start = document.getElementById('start').value;
+    const year = document.getElementById('year').value;
+
+    const daysByMonth = [];
+    for (let i = 0; i < 12; i++) {
+        const totalDays = new Date(year, i, 0).getDate()
+        daysByMonth.push(totalDays);
     }
 
-    const pares = [];
-    const impares = [];
-    pages.forEach(page => {
-        if (page % 2 == 0)
-            pares.push(page);
-        else
-            impares.push(page);
+    console.log(daysByMonth);
+
+    const pagesByMonth = [];
+
+    let pageStart = parseInt(start);
+    daysByMonth.forEach((days, index) => {
+        const month = dictMonth[index];
+        const pares = [];
+        const impares = [];
+
+        let currentPage = pageStart;
+        for (let i = 0; i < days; i++) {
+            if (currentPage % 2 == 0)
+                pares.push(currentPage);
+            else
+                impares.push(currentPage);
+
+            if (i === days - 1) pageStart = currentPage + 1;
+            currentPage++;
+        }
+
+        pagesByMonth.push({
+            month,
+            pares,
+            impares,
+        });
     });
 
-    paresInput.value = pares.join(',');
-    imparesInput.value = impares.join(',');
+    console.log(pagesByMonth);
+
+    let htmlResult = '';
+    pagesByMonth.forEach((item) => {
+        const htmlItem = `
+            <div class="row">
+                <div class="col-12">
+                    <h4 class="my-4">${item.month}</h4>
+                </div>
+                <div class="col-12">
+                    <label for="pares" class="form-label">Pares:</label>
+                    <input class="form-control" id="pares" type="text" readonly value="${item.pares.join(',')}" />
+                </div>
+                <div class="col-12">
+                    <label for="impares" class="form-label">Ímpares:</label>
+                    <input class="form-control" id="impares" type="text" readonly value="${item.impares.join(',')}" />
+                </div>
+            </div>
+        `;
+
+        htmlResult += htmlItem;
+    });
+
+    document.getElementById('result').innerHTML = htmlResult;
 });
